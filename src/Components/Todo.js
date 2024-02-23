a73384e1b44896fe0b45a08d41b7af54649a337a
Sruthysheen <sruthymolks@gmail.com>
2024-02-23 16:51:20 +0530

    third commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -19,34 +19,32 @@ function Todo() {
 
 
 
- const addTodo = () => {
-  if (todo.trim() !== '') {
-    const isDuplicate = todos.some((item) => item.list.toLowerCase() === todo.toLowerCase());
-
-    if (isDuplicate) {
-      setErrorMessage('Duplicate todo. Please enter a unique value.');
+  const addTodo = () => {
+    if (todo.trim() !== '') {
+      const isDuplicate = todos.some((item) => item.list.toLowerCase() === todo.toLowerCase());
+  
+      if (isDuplicate) {
+        setErrorMessage('Duplicate todo. Please enter a unique value.');
+      } else {
+        if (editId) {
+          const updatedTodos = todos.map((item) =>
+            item.id === editId ? { ...item, list: todo } : item
+          );
+          setTodos(updatedTodos);
+          setEditId(0);
+          setTodo('');
+          setErrorMessage('');
+        } else {
+          setTodos([{ list: todo, id: Date.now(), status: false }, ...todos]);
+          setTodo('');
+          setErrorMessage('');
+        }
+      }
     } else {
-      
-      setTodos([ { list: todo, id: Date.now(), status: false },...todos]);
-      console.log(todos);
-      setTodo('');
-      setErrorMessage('');
+      setErrorMessage('Please enter a non-empty value');
     }
-  } else {
-    setErrorMessage('Please enter a non-empty value');
-  }
-
-  if (editId) {
-    const editTodo = todos.find((item) => item.id === editId);
-    const updateTodo = todos.map((to) =>
-      to.id === editTodo.id ? { id: to.id, list: todo } : { id: to.id, list: to.list }
-    );
-    setTodos(updateTodo);
-    setEditId(0);
-    setTodo('');
-  }
-};
-
+  };
+  
 
   const inputRef = useRef("null");
 
